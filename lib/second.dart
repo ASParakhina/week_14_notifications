db5bddb232e86f7f5470dfb99b97262e5d6b174f
ASParakhina <alenka_parahina@mail.ru>
2022-11-15 20:15:41 +0300

    newcommit

--- lib/second.dart
+++ lib/second.dart
@@ -90,7 +90,7 @@ class _NotificationsPageState extends State<NotificationsPage> {
   tz.TZDateTime _nextInstanceOfTenAM() {
     final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
     tz.TZDateTime scheduledDate =
-        tz.TZDateTime(tz.local, now.year, now.month, now.day, 10);
+        tz.TZDateTime(tz.local, now.year, now.month, now.day, 21, 15);
     if (scheduledDate.isBefore(now)) {
       scheduledDate = scheduledDate.add(const Duration(days: 1));
     }
