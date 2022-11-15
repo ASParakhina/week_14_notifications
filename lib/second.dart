5725a179b2a39f74d54c57626d4960d1a0fd1a76
ASParakhina <alenka_parahina@mail.ru>
2022-11-15 22:11:55 +0300

    new_commit

--- lib/second.dart
+++ lib/second.dart
@@ -1,100 +1,106 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:timezone/timezone.dart' as tz;
-import 'package:timezone/data/latest.dart' as tz;
+// import 'package:flutter/material.dart';
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// import 'package:timezone/timezone.dart' as tz;
+// import 'package:timezone/data/latest.dart' as tz;
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: const NotificationsPage(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         primarySwatch: Colors.amber,
+//       ),
+//       home: const NotificationsPage(),
+//     );
+//   }
+// }
 
-class NotificationsPage extends StatefulWidget {
-  const NotificationsPage({Key? key}) : super(key: key);
+// class NotificationsPage extends StatefulWidget {
+//   const NotificationsPage({Key? key}) : super(key: key);
 
-  @override
-  State<NotificationsPage> createState() => _NotificationsPageState();
-}
+//   @override
+//   State<NotificationsPage> createState() => _NotificationsPageState();
+// }
 
-class _NotificationsPageState extends State<NotificationsPage> {
-//объект уведомления - экземпляр
-  late FlutterLocalNotificationsPlugin localNotifications;
+// class _NotificationsPageState extends State<NotificationsPage> {
+// //объект уведомления - экземпляр
+//   late FlutterLocalNotificationsPlugin localNotifications;
 
-  @override
-  void initState() {
-    super.initState();
+//   @override
+//   void initState() {
+//     super.initState();
 
-    //object for Android settings
-    const androidInitialize = AndroidInitializationSettings('ic_launcher');
-    //object for IOS settings
-    const iOSInitialize = DarwinInitializationSettings(
-      requestSoundPermission: true,
-      requestBadgePermission: true,
-      requestAlertPermission: true,
-    );
-    // all information
-    const initializationSettings =
-        InitializationSettings(android: androidInitialize, iOS: iOSInitialize);
+//     //object for Android settings
+//     const androidInitialize = AndroidInitializationSettings('ic_launcher');
+//     //object for IOS settings
+//     const iOSInitialize = DarwinInitializationSettings(
+//       requestSoundPermission: true,
+//       requestBadgePermission: true,
+//       requestAlertPermission: true,
+//     );
+//     // all information
+//     const initializationSettings =
+//         InitializationSettings(android: androidInitialize, iOS: iOSInitialize);
 
-    //we create local notofication
-    localNotifications = FlutterLocalNotificationsPlugin();
-    localNotifications.initialize(initializationSettings);
-  }
+//     //we create local notofication
+//     localNotifications = FlutterLocalNotificationsPlugin();
+//     localNotifications.initialize(initializationSettings);
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-        child: Text('Click the button to receive.'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: showEveryDay,
-        child: const Icon(Icons.notifications),
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: const Center(
+//         child: Text('Click the button to receive.'),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: showEveryDay,
+//         child: const Icon(Icons.notifications),
+//       ),
+//     );
+//   }
 
-  Future<NotificationDetails> _showNotification() async {
-    tz.initializeTimeZones();
-    const androidDetails = AndroidNotificationDetails("ID", "Name notification",
-        importance: Importance.high,
-        channelDescription: "content notifocation");
+//   Future<NotificationDetails> _showNotification() async {
+//     tz.initializeTimeZones();
+//     const androidDetails = AndroidNotificationDetails("ID", "Name notification",
+//         importance: Importance.high,
+//         channelDescription: "content notifocation");
 
-    const iosDetails = DarwinNotificationDetails();
-    return const NotificationDetails(android: androidDetails, iOS: iosDetails);
-  }
+//     const iosDetails = DarwinNotificationDetails();
+//     return const NotificationDetails(android: androidDetails, iOS: iosDetails);
+//   }
 
-  Future<void> showEveryDay() async {
-    final details = await _showNotification();
+//   Future<void> showEveryDay() async {
+//     final details = await _showNotification();
+//     tz.initializeTimeZones();
 
-    // var moscow = tz.getLocation('Russia/Moscow');
-    await localNotifications.zonedSchedule(
-        0, 'Name', 'body', _nextInstanceOfTenAM(), details,
-        uiLocalNotificationDateInterpretation:
-            UILocalNotificationDateInterpretation.absoluteTime,
-        matchDateTimeComponents: DateTimeComponents.time,
-        androidAllowWhileIdle: true);
-  }
+//     // var moscow = tz.getLocation('Russia/Moscow');
+//     await localNotifications.zonedSchedule(
+//       0,
+//       'Name',
+//       'body',
+//       _nextInstanceOfTenAM(),
+//       details,
+//       androidAllowWhileIdle: true,
+//       uiLocalNotificationDateInterpretation:
+//           UILocalNotificationDateInterpretation.absoluteTime,
+//       matchDateTimeComponents: DateTimeComponents.time,
+//     );
+//   }
 
-  tz.TZDateTime _nextInstanceOfTenAM() {
-    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
-    tz.TZDateTime scheduledDate =
-        tz.TZDateTime(tz.local, now.year, now.month, now.day, 8, 25);
-    if (scheduledDate.isBefore(now)) {
-      scheduledDate = scheduledDate.add(const Duration(days: 1));
-    }
-    return scheduledDate;
-  }
-}
+//   tz.TZDateTime _nextInstanceOfTenAM() {
+//     final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+//     tz.TZDateTime scheduledDate =
+//         tz.TZDateTime(tz.local, now.year, now.month, now.day, 8, 40);
+//     if (scheduledDate.isBefore(now)) {
+//       scheduledDate = scheduledDate.add(const Duration(days: 1));
+//     }
+//     return scheduledDate;
+//   }
+// }
